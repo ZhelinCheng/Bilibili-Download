647aa6d9f4db3dd73d2d72a019498da5bf257075
ZhelinCheng <chengzhelin@foxmail.com>
2021-01-08 18:01:20 +0800

    保存

--- main/helpers/database.ts
+++ main/helpers/database.ts
@@ -2,16 +2,29 @@
  * @Author       : Zhelin Cheng
  * @Date         : 2021-01-08 17:38:21
  * @LastEditors  : Zhelin Cheng
- * @LastEditTime : 2021-01-08 17:49:01
+ * @LastEditTime : 2021-01-08 17:58:51
  * @FilePath     : /Bilibili-Download/main/helpers/database.ts
  * @Description  : 未添加文件描述
  */
 
-import low from 'lowdb'
+import Datastore from 'lowdb'
 import FileSync from 'lowdb/adapters/FileSync'
-import { resolve } from 'path'
-const adapter = new FileSync(resolve(__dirname, '../../db.json'))
-const db = low(adapter)
+import path from 'path'
+import fs from 'fs-extra'
+import { remote, app } from 'electron'
+
+const APP = process.type === 'renderer' ? remote.app : app
+
+const STORE_PATH = APP.getPath('userData')
+
+if (process.type !== 'renderer') {
+  if (!fs.pathExistsSync(STORE_PATH)) {
+    fs.mkdirpSync(STORE_PATH)
+  }
+}
+
+const adapter = new FileSync(path.join(STORE_PATH, '/101helper.json'))
+const db = Datastore(adapter)
 
 db.defaults({
   SESSDATA: '',
